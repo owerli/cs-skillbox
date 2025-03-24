5e3957bf3760126a199dcb06b457f664be7eb0e7
~Alexander Platonov <platonov1058b84@gmail.com>
2025-03-24 19:14:22 +0300

    homework 5.5

---
+++ cs-skillbox/5/FirstTask/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input your sentence: ");
+            string sentence = Console.ReadLine();
+
+            PrintWords(
+                SplitText(sentence)    
+            );
+
+        }
+
+        static string[] SplitText(string line)
+        {
+
+            line = line.Trim();
+
+            int spaceCount = 0;
+
+            bool isPeviousSymbolSpace = false;
+            foreach (char symbol in line)
+            {
+
+                if (symbol == ' ' && isPeviousSymbolSpace)
+                {
+                    continue;
+                }
+                else if (symbol == ' ')
+                {
+                    isPeviousSymbolSpace = true;
+                    spaceCount++;
+                }
+                else
+                {
+                    isPeviousSymbolSpace = false;
+                }
+
+            }
+
+            string[] words = new string[spaceCount + 1];
+
+            spaceCount = 0;
+            string word = "";
+            foreach (char symbol in line)
+            {
+
+                if (symbol == ' ' && isPeviousSymbolSpace)
+                {
+                    continue;
+                }
+                else if (symbol == ' ')
+                {
+                    isPeviousSymbolSpace = true;
+                    words[spaceCount] = word;
+                    word = "";
+                    spaceCount++;
+                }
+                else
+                {
+                    isPeviousSymbolSpace = false;
+                    word += symbol;
+                }
+
+            }
+
+            if (word != "") words[words.Length - 1] = word;
+
+            return words;
+
+        }
+
+        static void PrintWords(string[] words) 
+        {
+
+            foreach (string word in words) {
+            
+                Console.WriteLine(word);
+
+            }
+
+        } 
+
+    }
+}
---
+++ cs-skillbox/5/SecondTask/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input your sentence: ");
+            string sentence = Console.ReadLine();
+
+            ReverseWords(sentence);
+
+        }
+
+        static string[] SplitText(string line)
+        {
+
+            line = line.Trim();
+
+            int spaceCount = 0;
+
+            bool isPeviousSymbolSpace = false;
+            foreach (char symbol in line)
+            {
+
+                if (symbol == ' ' && isPeviousSymbolSpace)
+                {
+                    continue;
+                }
+                else if (symbol == ' ')
+                {
+                    isPeviousSymbolSpace = true;
+                    spaceCount++;
+                }
+                else
+                {
+                    isPeviousSymbolSpace = false;
+                }
+
+            }
+
+            string[] words = new string[spaceCount + 1];
+
+            spaceCount = 0;
+            string word = "";
+            foreach (char symbol in line)
+            {
+
+                if (symbol == ' ' && isPeviousSymbolSpace)
+                {
+                    continue;
+                }
+                else if (symbol == ' ')
+                {
+                    isPeviousSymbolSpace = true;
+                    words[spaceCount] = word;
+                    word = "";
+                    spaceCount++;
+                }
+                else
+                {
+                    isPeviousSymbolSpace = false;
+                    word += symbol;
+                }
+
+            }
+
+            if (word != "") words[words.Length - 1] = word;
+
+            return words;
+
+        }
+
+        static string Reverse(string text)
+        {
+
+            string[] words = SplitText(text);
+            string reversedSentence = "";
+            for (int wordIndex = words.Length - 1; wordIndex >= 0; wordIndex--)
+            {
+
+                reversedSentence += words[wordIndex] + (wordIndex == 0 ? "" : " ");
+
+            }
+
+            return reversedSentence;
+        
+        }
+
+        static void ReverseWords(string sentence) 
+        {
+
+            Console.WriteLine(
+                Reverse(sentence)    
+            );
+        
+        }
+
+    }
+}
